993756c43a7a80e3bca874f18f76fe81efcad441
Barna Udvarhelyi <barna.udvarhelyi@gmail.com>
2023-05-02 18:43:27 +0200

    File sys refact try

--- Backend/src/main/java/mine/homeworkproject/AppMain.java
+++ Backend/src/main/java/mine/homeworkproject/AppMain.java
@@ -31,7 +31,7 @@ public class AppMain implements CommandLineRunner {
   public void run(String... args) throws Exception {
 //    userRepository.save(new User("Admin", "admin@gmail.com", passwordEncoder.encode("Admin"), "Admin"));
 //    userRepository.save(new User("User", "user@gmail.com", passwordEncoder.encode("User"), "User"));
-//
+
 //    productService.getRandomProductsFromAPI();
     System.out.println("---------- App has been started! ----------");
   }
